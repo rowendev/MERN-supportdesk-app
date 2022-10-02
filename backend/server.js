2b7131c441a4786d1db0aab619676fa8aa6a8686
rowendev <rowendevtw@gmail.com>
2022-10-02 15:51:18 +0800

    work on backend auth functions

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
 const errorHandler = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+// connect database
+connectDB();
 
 const app = express();
 
