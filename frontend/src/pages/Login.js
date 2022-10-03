1e3d60b756ce1c72d85b36e810d9de15511fcbc8
rowendev <rowendevtw@gmail.com>
2022-10-03 22:06:28 +0800

    add some styling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,6 +3,8 @@ import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
 import { login } from "../features/auth/authSlice";
+import LoginImg from "../assets/login.svg";
+import { Link } from "react-router-dom";
 
 function Login() {
   const [fromData, serFormData] = useState({
@@ -39,38 +41,50 @@ function Login() {
         <h4>登入</h4>
         <h5>Enter your email and password</h5>
       </section>
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              className="form-control"
-              value={email}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="form-control"
-              value={password}
-              onChange={onChange}
-              required
-            />
-          </div>
+      <div style={{ display: "flex" }}>
+        <section>
+          <img
+            src={LoginImg}
+            alt="image"
+            style={{ width: "300px", height: "300px" }}
+          />
+        </section>
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="form-control"
+                value={email}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="form-control"
+                value={password}
+                onChange={onChange}
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <button className="btn btn-block">送出</button>
-          </div>
-        </form>
-      </section>
+            <div className="form-group">
+              <button className="btn btn-block">送出</button>
+            </div>
+            <Link to="/signup" style={{ textDecoration: "underline" }}>
+              沒有帳號? 這邊註冊
+            </Link>
+          </form>
+        </section>
+      </div>
     </>
   );
 }
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
 import { register, reset } from "../features/auth/authSlice";
+import SignupImg from "../assets/signup.svg";
 
 function Signup() {
   const [fromData, serFormData] = useState({
@@ -60,61 +61,73 @@ function Signup() {
         <h4>註冊</h4>
         <h5>Please create an account</h5>
       </section>
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="form-control"
-              value={name}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              className="form-control"
-              value={email}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="form-control"
-              value={password}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password2">Confirm password:</label>
-            <input
-              type="password"
-              id="password2"
-              name="password2"
-              className="form-control"
-              value={password2}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <button className="btn btn-block">送出</button>
-          </div>
-        </form>
-      </section>
+      <div style={{ display: "flex" }}>
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control"
+                value={name}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="form-control"
+                value={email}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="form-control"
+                value={password}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password2">Confirm password:</label>
+              <input
+                type="password"
+                id="password2"
+                name="password2"
+                className="form-control"
+                value={password2}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-block">送出</button>
+            </div>
+            <Link to="/login" style={{ textDecoration: "underline" }}>
+              已經有帳號了? 這邊登入
+            </Link>
+          </form>
+        </section>
+        <section style={{ margin: "auto 0" }}>
+          <img
+            src={SignupImg}
+            alt="image"
+            style={{ width: "300px", height: "300px" }}
+          />
+        </section>
+      </div>
     </>
   );
 }
